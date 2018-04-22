e1c348757ebc8454887b4727b4e63828321b0a65
morgan-nanez <morgan.nanez@gmail.com>
2018-04-22 15:05:18 -0700

    wrote code for spinAndFind

--- ProbeCircuit/ProbeCircuit.ino
+++ ProbeCircuit/ProbeCircuit.ino
@@ -11,6 +11,8 @@ void setup()
  pinMode(analogPin, INPUT);
  pinMode(strobePin, OUTPUT);
  pinMode(resetPin, OUTPUT);
+ pinMode(rightMotorPin, OUTPUT);
+ pinMode(leftMotorPin, OUTPUT);
  analogReference(DEFAULT);
 
  digitalWrite(resetPin, LOW);
@@ -22,17 +24,22 @@ void loop()
  digitalWrite(resetPin, HIGH);
  digitalWrite(resetPin, LOW);
 
+ //the car starts to move
+ digitalWrite(rightMotorPin, HIGH);
+ digitalWrite(leftMotorPin, HIGH);
+ 
+
  for (int i = 0; i < 7; i++)
  {
- digitalWrite(strobePin, LOW);
- delayMicroseconds(30); // to allow the output to settle
- spectrumValue[i] = (5.0 / 1023.0) * analogRead(analogPin);
-
- Serial.print(i);
- Serial.print(":");
- Serial.print(spectrumValue[i] * (5.0 / 1023.0));
- Serial.print(" ");
- //}
+   digitalWrite(strobePin, LOW);
+   delayMicroseconds(30); // to allow the output to settle
+   spectrumValue[i] = (5.0 / 1023.0) * analogRead(analogPin);
+  
+   Serial.print(i);
+   Serial.print(":");
+   Serial.print(spectrumValue[i] * (5.0 / 1023.0));
+   Serial.print(" ");
+    }
 
  //checks to see if any of the values are in the frequencey range
 for (int i : spectrumValue) {
@@ -41,15 +48,43 @@ for (int i : spectrumValue) {
     spinAndFind();
   }
 
- digitalWrite(strobePin, HIGH);
- }
- Serial.println();
+  digitalWrite(strobePin, HIGH);
+  }
+  Serial.println();
 }
 
 void spinAndFind()
 {
-  
-}
+  //stops car
+   digitalWrite(rightMotorPin, LOW);
+   digitalWrite(leftMotorPin, LOW);
+
+   double highest = 0; //highest frequencey found
+   
+   unsigned long start = millis();
+   unsigned long timeToSpin = 0;
+
+   
+   digitalWrite(rightMotorPin, HIGH);
+   for (int i = 0; i < 7; i++)
+   {
+    if (spectrunValue[i] > highest) {
+      highest = spectrumValue[i]  
+      timeToSpin = millis();  
+      }
+   }
+
+   long spin = start - timeToStart;
+   while (spin >= 0) 
+   {
+       digitalWrite(rightMotorPin, HIGH);
+       spin = spin - 1;
+   }
+   
+   digitalWrite(rightMotorPin, LOW);
+   digitalWrite(leftMotorPin, LOW);
+   loop()
+   }
 
 }
 
