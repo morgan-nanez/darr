e270de9b22f1b681a6bc5b6aa1c7785e4f264b37
Michael Hole <mhole44@berkeley.edu>
2018-04-30 15:43:11 -0700

    changes

--- ProbeCircuit/ProbeCircuit.ino
+++ ProbeCircuit/ProbeCircuit.ino
@@ -1,37 +1,57 @@
 #include <AFMotor.h>
-AF_DCMotor motor(4);
+AF_DCMotor motor(2);
+
+const int ledPin =  23; // the number of the LED pin
+const int redLED = 22;
 
 int analogPin = 7; // read from multiplexer using analog input 0
 int strobePin = 52; // strobe is attached to digital pin 2
 int resetPin = 53; // reset is attached to digital pin 3
-float spectrumValue[7]; // to hold a2d values
 float spectrumVals[2]; // two values we need
-int DELAY = 3400;
+unsigned long DELAY = 3620, LEDDelay = 200;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(analogPin, INPUT);
+  //pinMode(analogPin, INPUT);
+  analogReference(DEFAULT);
   pinMode(strobePin, OUTPUT);
   pinMode(resetPin, OUTPUT);
-  analogReference(DEFAULT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  
 
   digitalWrite(resetPin, LOW);
   digitalWrite(strobePin, HIGH);
 
-  motor.setSpeed(200);
+  motor.setSpeed(LEDDelay);
 }
 
 void loop() {
-  spinAndFind();
-  //setVals();
+  // Blink twice
+  //digitalWrite(ledPin, HIGH);
+  //delay(LEDDelay);
+  //digitalWrite(ledPin, LOW);
+  //delay(LEDDelay);
+  //digitalWrite(ledPin, HIGH);
+  //delay(LEDDelay);
+  //digitalWrite(ledPin, LOW);
 
-  //checks to see if any of the values are in the frequencey range
-  if (spectrumVals[0] > 3.0 || spectrumVals[1] > 3.0) {
-    spin();
-  }
+  // Wait 1/2 second
+  delay(500);
+  
+  spinAndFind();
+  delay(7000);
 }
 
 void setVals() {
+
+  // Blink red LED once
+  //digitalWrite(redLED, HIGH);
+  //delay(LEDDelay);
+  //digitalWrite(redLED, LOW);
+
+  // Wait 1/2 second
+  
   digitalWrite(resetPin, HIGH);
   digitalWrite(resetPin, LOW);
    
@@ -41,23 +61,13 @@ void setVals() {
     delayMicroseconds(30); // to allow the output to settle
     if (i == 2 || i == 3) {
       spectrumVals[i-2] = (5.0 / 1023.0) * analogRead(analogPin);
-      spectrumValue[i] = spectrumVals[i-2];
-    } else {
-      spectrumValue[i] = (5.0 / 1023.0) * analogRead(analogPin);
-    }
-    
-    Serial.print(i);
-    Serial.print(": SEVEN VAL: ");
-    Serial.print(spectrumValue[i]);
-    Serial.print(" ");
-  
-    Serial.println();
-     
+      Serial.println(spectrumVals[i-2]);
+    }  
     digitalWrite(strobePin, HIGH);
-   }
+  }
 }
 
-void spin(int duration) {
+void spin_duration(int duration) {
   Serial.print("SPIN DURATION:");
   Serial.println(duration);
   motor.run(FORWARD);
@@ -76,13 +86,29 @@ void spin() {
    there. Predicates on the assumption that DELAY creates one 
    full revolution for the bot. */
 void spinAndFind() {
-  float topAvg = 0;
+  // Blink 3 times
+  //digitalWrite(ledPin, HIGH);
+  //delay(LEDDelay);
+  //digitalWrite(ledPin, LOW);
+  //delay(LEDDelay);
+  //digitalWrite(ledPin, HIGH);
+  //delay(LEDDelay);
+  //digitalWrite(ledPin, LOW);
+  //delay(LEDDelay);
+  //digitalWrite(ledPin, HIGH);
+  //delay(LEDDelay);
+  //digitalWrite(ledPin, LOW);
+  
+  // Wait 1/2 second
+  delay(500);
+  float topAvg = -1;
   int howFar = 0, numIntervals = 10;
   Serial.println("SPIN AND FIND");
   
   // Find what direction had the greatest signal.
   for (int i = 0; i < numIntervals; i++) {
     motor.run(FORWARD);
+    Serial.println(DELAY / numIntervals);
     delay(DELAY / numIntervals);
     motor.run(RELEASE);
     
@@ -93,6 +119,7 @@ void spinAndFind() {
       howFar = i;
     }
   }
+  delay(1000);
 
   // Go back to the direction of the greatest signal.
   motor.run(FORWARD);
@@ -101,39 +128,4 @@ void spinAndFind() {
   delay(DELAY * (howFar / numIntervals));
   motor.run(RELEASE);
 }
-
-/*
-void spinAndFind()
-{
-  //stops car
-   digitalWrite(rightMotorPin, LOW);
-   digitalWrite(leftMotorPin, LOW);
-
-   double highest = 0; //highest frequencey found
-   
-   unsigned long start = millis();
-   unsigned long timeToSpin = 0;
-
-   
-   digitalWrite(rightMotorPin, HIGH);
-   for (int i = 0; i < 7; i++)
-   {
-    if (spectrumValue[i] > highest) {
-      highest = spectrumValue[i]  
-      timeToSpin = millis();  
-      }
-   }
-
-   long spin = start - timeToStart;
-   while (spin >= 0) 
-   {
-       digitalWrite(rightMotorPin, HIGH);
-       spin = spin - 1;
-   }
-   
-   digitalWrite(rightMotorPin, LOW);
-   digitalWrite(leftMotorPin, LOW);
-   loop()
-   }
-*/
 
